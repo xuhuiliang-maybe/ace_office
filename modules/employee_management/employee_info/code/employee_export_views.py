28055e74edb16a121f39b15ed7be76015a36c829
xuhuiliang <15011544971@163.com>
2018-05-15 00:30:14 +0800

    add

--- modules/employee_management/employee_info/code/employee_export_views.py
+++ modules/employee_management/employee_info/code/employee_export_views.py
@@ -567,10 +567,9 @@ class NewEmployeeExportView(View):
 				kwargs.update({"is_temporary": True})
 			employee_obj_list = Employee.objects.filter(**kwargs)
 
-
 			if employee_obj_list.exists():
 				# 组装导出数据
-				# write_employee_file(employee_type, employee_obj_list, filepath)
+				write_employee_file(employee_type, employee_obj_list, filepath)
 				download_path = "链接:https://pan.baidu.com/s/1Y7PWQ2gzfte5f7ELtK3tgg  密码:169c"
 				messages.success(self.request, u"成功导出员工信息（%s）, %s"% (file_name, download_path))
 			else:
