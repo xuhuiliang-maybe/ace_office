95d1fb9fee1f348e162a6c13fa799f3564c6714d
xuhuiliang-maybe <15011544971@163.com>
2019-03-18 10:13:34 +0800

    修改员工信息创建时间

--- modules/employee_management/employee_info/models.py
+++ modules/employee_management/employee_info/models.py
@@ -197,7 +197,7 @@ class Employee(models.Model):
     remark3 = models.CharField(u"备注3", max_length=256, blank=True)
     remark4 = models.CharField(u"备注4", max_length=256, blank=True)
     remark5 = models.CharField(u"备注5", max_length=256, blank=True)
-    create_time = models.DateTimeField(verbose_name=u'创建时间', auto_now=True, blank=True, null=True, db_index=True)
+    create_time = models.DateTimeField(verbose_name=u'创建时间', auto_now_add=True, blank=True, null=True, db_index=True)
 
     def __str__(self):
         return self.name
