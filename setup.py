e3281d4adb72a2041924449c14f8357a4490fe45
xuhuiliang-maybe <15011544971@163.com>
2019-04-19 10:19:55 +0800

    test pip install ace_office

---
+++ setup.py
@@ -0,0 +1,23 @@
+# encoding: utf-8
+from setuptools import setup
+
+setup(
+    name="ace_office",
+    version="1.0",
+    keywords=("ace_office", "xxx"),
+    description="eds sdk",
+    long_description="eds sdk for python",
+    license="MIT Licence",
+
+    url="http://test.com",
+    author="test",
+    author_email="test@gmail.com",
+
+    packages=['modules'],
+    include_package_data=True,
+    platforms="any",
+    install_requires=[],
+
+    scripts=[],
+    entry_points={}
+)
