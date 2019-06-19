5bfd0ab4e226465811cf5fa1e9c926b7b5ae5c3e
root <root@iZ2zefjy4361l1rzclhrnmZ.(none)>
2019-06-19 15:17:06 +0800

    change uwsgi to supervisor and gunicorn

---
+++ gunicorn_config.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+import multiprocessing
+
+preload_app = True
+chdir = '/var/www/html/ace_office'
+bind = "127.0.0.1:9002"
+workers = multiprocessing.cpu_count()*6
+max_requests = 5000
+user = 'root'
+group = 'root'
+#accesslog = '/var/log/gunicorn/xuetangx_video_log_access.log'
+#errorlog = '/var/log/gunicorn/xuetangx_video_log_error.log'
