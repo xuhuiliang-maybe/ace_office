45405a60974b5994a1a0dca09ffaac5bfd1cdaed
xuhuiliang-maybe <15011544971@163.com>
2019-06-26 15:07:33 +0800

    修改进程数

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -4,9 +4,9 @@ import multiprocessing
 preload_app = True
 chdir = '/var/www/html/ace_office'
 bind = "127.0.0.1:9002"
-workers = multiprocessing.cpu_count()*6
+workers = multiprocessing.cpu_count() * 2 + 1
 max_requests = 5000
 user = 'root'
 group = 'root'
-#accesslog = '/var/log/gunicorn/xuetangx_video_log_access.log'
-#errorlog = '/var/log/gunicorn/xuetangx_video_log_error.log'
+# accesslog = '/var/log/gunicorn/xuetangx_video_log_access.log'
+# errorlog = '/var/log/gunicorn/xuetangx_video_log_error.log'
