a79ce73f7711a5a5492e49e2ed40a8effd4f8671
xuhuiliang-maybe <15011544971@163.com>
2019-08-12 14:46:02 +0800

    增加日志处理

--- ace_office/settings.py
+++ ace_office/settings.py
@@ -68,7 +68,7 @@ INSTALLED_APPS = (
     'modules.social_security.social_security_detail',  # 社保明细
     'modules.settlement_pay',  # 结算发薪
     'modules.recruitment_manage',  # 招聘管理
-
+    
     'modules.approval_process',  # 审批流程
     'modules.approval_process.leave',  # 审批流程，请假
     'modules.approval_process.loan',  # 审批流程，备用金
@@ -79,7 +79,7 @@ INSTALLED_APPS = (
     'modules.approval_process.demand_turnover',  # 审批流程，管理人员需求与离职
     'modules.approval_process.temporary_write_offs_billing',  # 审批流程，临时工销账与开票
     'modules.approval_process.recruited_billing',  # 审批流程，待招结算与开票
-
+    
     'modules.expense_manage',  # 费用管理
     'modules.personnel_operation',  # 人事操作质量
     'modules.system',  # 系统管理
@@ -88,13 +88,13 @@ INSTALLED_APPS = (
     'modules.sidebar_menu',  # 页面菜单模块
     'modules.payroll_manage.payroll_detail',  # 薪资明细
     'modules.payroll_manage.payroll_gather',  # 薪资汇总
-
+    
     'modules.finance.social_security_audit',  # 财务-社保审核
     'modules.finance.arrival_and_billing',  # 财务-到账与开票
     'modules.finance.loans_and_write_offs',  # 财务-借款与销账
-
+    
     'modules.contract_manage',  # 合同
-
+    
     # 'modules.attendance',  # 考勤
 )
 
@@ -213,4 +213,85 @@ CACHES = {
             # 缓存到达最大个数之后，Django会自动清空3/1 ,设置为10 就剔除10/1
         }
     }
+}
+
+BASE_LOG_DIR = os.path.join(BASE_DIR, "log")
+LOGGING = {
+    'version': 1,  # 保留字
+    'disable_existing_loggers': False,  # 禁用已经存在的logger实例
+    # 日志文件的格式
+    'formatters': {
+        # 详细的日志格式
+        'standard': {
+            'format': '[%(asctime)s][%(threadName)s:%(thread)d][task_id:%(name)s][%(filename)s:%(lineno)d]'
+                      '[%(levelname)s][%(message)s]'
+        },
+        # 简单的日志格式
+        'simple': {
+            'format': '[%(levelname)s][%(asctime)s][%(filename)s:%(lineno)d]%(message)s'
+        },
+        # 定义一个特殊的日志格式
+        'collect': {
+            'format': '%(message)s'
+        }
+    },
+    # 过滤器
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    # 处理器
+    'handlers': {
+        # 在终端打印
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],  # 只有在Django debug为True时才在屏幕打印日志
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        # 默认的
+        'default': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件，自动切
+            'filename': os.path.join(BASE_LOG_DIR, "ace_office_info.log"),  # 日志文件
+            'maxBytes': 1024 * 1024 * 50,  # 日志大小 50M
+            'backupCount': 3,  # 最多备份几个
+            'formatter': 'standard',
+            'encoding': 'utf-8',
+        },
+        # 专门用来记错误日志
+        'error': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件，自动切
+            'filename': os.path.join(BASE_LOG_DIR, "ace_office_err.log"),  # 日志文件
+            'maxBytes': 1024 * 1024 * 50,  # 日志大小 50M
+            'backupCount': 5,
+            'formatter': 'standard',
+            'encoding': 'utf-8',
+        },
+        # 专门定义一个收集特定信息的日志
+        'collect': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',  # 保存到文件，自动切
+            'filename': os.path.join(BASE_LOG_DIR, "ace_office_collect.log"),
+            'maxBytes': 1024 * 1024 * 50,  # 日志大小 50M
+            'backupCount': 5,
+            'formatter': 'collect',
+            'encoding': "utf-8"
+        }
+    },
+    'loggers': {
+        # 默认的logger应用如下配置
+        '': {
+            'handlers': ['default', 'console', 'error'],  # 上线之后可以把'console'移除
+            'level': 'DEBUG',
+            'propagate': True,  # 向不向更高级别的logger传递
+        },
+        # 名为 'collect'的logger还单独处理
+        'collect': {
+            'handlers': ['console', 'collect'],
+            'level': 'INFO',
+        }
+    },
 }
